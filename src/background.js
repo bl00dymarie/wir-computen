efa9db53d8e27bab7cd810db6582d47aa5679f3c
emelko <ml.kochsiek@mailbox.org>
2020-04-07 17:58:37 +0200

    Test

---
+++ src/background.js
@@ -0,0 +1,21 @@
+class Background {
+    constructor() {
+        this.imgs = [
+            // { src: loadImage("../assets/parallax-mountain-bg.png"), x: 0, speed: 0 },
+            { src: loadImage("../assets/parallax-mountain-foreground-trees.png"), x: 0, speed: 0 },
+            { src: loadImage("../assets/parallax-mountain-montain-far.png"), x: 0, speed: 0 },
+            { src: loadImage("../assets/parallax-mountain-mountains.png"), x: 0, speed: 0 },
+            { src: loadImage("../assets/parallax-mountain-trees.png"), x: 0, speed: 0 },
+        ]
+    }
+
+    show(img) {
+        image(img.src, 0,0)
+    }
+
+    display() {
+        this.imgs.forEach(image => {
+            this.show(image)
+        })
+    }
+}
