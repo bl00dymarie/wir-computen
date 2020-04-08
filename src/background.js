04f6f5f7eedb56dcd8c5a2fb9a107ec24e928960
emelko <ml.kochsiek@mailbox.org>
2020-04-08 21:15:06 +0200

    Make the background layers move at different speed

--- src/background.js
+++ src/background.js
@@ -1,19 +1,17 @@
 class Background {
     constructor() {
-        this.random = 0
         this.imgs = [
-            { src: loadImage("../assets/layers-hill/Hills Layer 01.png"), x: 0, speed: 0 },
-            { src: loadImage("../assets/layers-hill/Hills Layer 02.png"), x: 0, speed: 0 },
-            { src: loadImage("../assets/layers-hill/Hills Layer 03.png"), x: 0, speed: 0 },
-            { src: loadImage("../assets/layers-hill/Hills Layer 04.png"), x: 0, speed: 0 },
-            { src: loadImage("../assets/layers-hill/Hills Layer 05.png"), x: 0, speed: 0 },
-            { src: loadImage("../assets/layers-hill/Hills Layer 06.png"), x: 0, speed: 0 },
+            { src: loadImage("../assets/layers-hill/Hills Layer 01.png"), x: 0, speed: 0.5 },
+            { src: loadImage("../assets/layers-hill/Hills Layer 02.png"), x: 0, speed: 0.8 },
+            { src: loadImage("../assets/layers-hill/Hills Layer 03.png"), x: 0, speed: 1 },
+            { src: loadImage("../assets/layers-hill/Hills Layer 04.png"), x: 0, speed: 1.5 },
+            { src: loadImage("../assets/layers-hill/Hills Layer 05.png"), x: 0, speed: 3 },
+            { src: loadImage("../assets/layers-hill/Hills Layer 06.png"), x: 0, speed: 3.5 },
         ]
     }
 
     move(img) {
         img.x -= img.speed
-        // image(img.src, 0,0)
         // image(we get the source of image, x coordinate, y coordinate )
         image(img.src, img.x, 0);
         image(img.src, img.x + width, 0);
